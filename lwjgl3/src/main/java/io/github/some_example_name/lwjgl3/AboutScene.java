5227fd490f6bdc37b418174ba6f67d1385b6b913
comt123 <aishwaryaprem2707@gmail.com>
2025-03-11 00:45:37 +0800

    Edited About us page

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/AboutScene.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/AboutScene.java
@@ -7,6 +7,7 @@ public class AboutScene extends Scene {
     private BackgroundEntity background;
     private CustomButton dismissButton;
     private CustomButton aboutButton;
+    private CustomButton infoButton;
     private CustomButton settingsButton;
     private Word word;
     private Word word1;
@@ -24,14 +25,15 @@ public class AboutScene extends Scene {
         float screenHeight = Gdx.graphics.getHeight();
 
         // Adjusted Text Positioning for Centering
-        float textX = screenWidth / 2 - 400;  // Center align text
-        float textY = screenHeight / 2 + 200;
+        float textX = screenWidth / 2 - 365;  // Center align text
+        float textY = screenHeight / 2 - 50;
 
+        // CHECK IF NEED TO CENTRALISE
         // Increase Font Size by Changing `scale` (Larger Numbers = Bigger Text)
         word = new Word(textX, textY, 0.1f, Color.BLACK, 
-                        "OverTrashed is a fun and educational game!Avoid enemies &\n save the environment!", 2.0f);
+                        "OverTrashed is a fun and educational game! \nAvoid enemies & save the environment!", 2.0f);
         word1 = new Word(textX, textY - 80, 0.1f, Color.BLACK, 
-                        "Credits: (P2T2) Gregory, Cavell, Wenjing, Royston, Aish, Luke", 2.0f);
+                        "Credits: (P2T2) \nGregory, Cavell, Wenjing, Royston, Aish, Luke", 2.0f);
 
         this.entityManager.addWord(word);
         this.entityManager.addWord(word1);
@@ -41,7 +43,7 @@ public class AboutScene extends Scene {
 //        float screenHeight = Gdx.graphics.getHeight();
 
         // Load Background Image
-        background = new BackgroundEntity("about_bg.png", 0, 0);
+        background = new BackgroundEntity("about_bg2.png", 0, 0);
         this.entityManager.addEntity(background);
 
         // Dismiss Button - Dimensions and Position as Percentage of Screen
@@ -56,6 +58,20 @@ public class AboutScene extends Scene {
         });
         this.entityManager.addEntity(dismissButton);
         this.ioManager.getInputManager().registerClickable(dismissButton);
+        
+        // Play Button - Dimensions and Position as Percentage of Screen
+        float buttonWidth1 = screenWidth * 0.15f;  // 25% of screen width
+        float buttonHeight1 = screenHeight * 0.05f;  // 8% of screen height
+        float buttonX1 = screenWidth * 0.45f - (buttonWidth)/2;  // Centered horizontally (50% - 25%/2)
+        float buttonY1 = screenHeight * 0.27f - (buttonHeight / 2);  // Centered vertically (50% - buttonHeight/2)
+
+        infoButton = new CustomButton("instructions_button.png", buttonX1, buttonY1, buttonWidth1, buttonHeight1);
+        // LINK THE INSTRUCTION BUTTON
+//        infoButton.setOnClickAction(() -> {
+//            this.sceneManager.setScene(GameScene.class);
+//        });
+        this.entityManager.addEntity(infoButton);
+//        this.ioManager.getInputManager().registerClickable(infoButton);
 //
 //        // Additional buttons can be defined similarly using relative dimensions.
 //        
