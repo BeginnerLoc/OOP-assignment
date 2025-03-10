39a11dd1cc4dddaa53b9ccd7740848ae07d20685
comt123 <aishwaryaprem2707@gmail.com>
2025-03-10 23:38:11 +0800

    fixing font size

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/AboutScene.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/AboutScene.java
@@ -1,12 +1,15 @@
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
 public class AboutScene extends Scene {
     private BackgroundEntity background;
     private CustomButton dismissButton;
     private CustomButton aboutButton;
     private CustomButton settingsButton;
+    private Word word;
+    private Word word1;
 
 
     public AboutScene(String name) {
@@ -16,10 +19,26 @@ public class AboutScene extends Scene {
     @Override
     public void create() {
         super.create();
-
-        // Get screen dimensions
+        
         float screenWidth = Gdx.graphics.getWidth();
         float screenHeight = Gdx.graphics.getHeight();
+
+        // ✅ Adjusted Text Positioning for Centering
+        float textX = screenWidth / 2 - 250;  // Center align text
+        float textY = screenHeight / 2 + 80;
+
+        // ✅ Increase Font Size by Changing `scale` (Larger Numbers = Bigger Text)
+        word = new Word(textX, textY, 0.1f, Color.WHITE, 
+                        "OverTrashed is a fun and educational game!\nAvoid enemies & save the environment!", 2.5f);
+        word1 = new Word(textX, textY - 80, 0.1f, Color.WHITE, 
+                        "Credits: (P2T2) Gregory, Cavell, Wenjing, Royston, Aish, Luke", 2.0f);
+
+        this.entityManager.addWord(word);
+        this.entityManager.addWord(word1);
+
+//        // Get screen dimensions
+//        float screenWidth = Gdx.graphics.getWidth();
+//        float screenHeight = Gdx.graphics.getHeight();
 
         // Load Background Image
         background = new BackgroundEntity("about_bg.png", 0, 0);
--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Word.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Word.java
@@ -1,26 +1,64 @@
+//package io.github.some_example_name.lwjgl3;
+//
+//import com.badlogic.gdx.graphics.Color;
+//import com.badlogic.gdx.math.Rectangle;
+//
+//public class Word extends Entity {
+//
+//	// string
+//	private String word;
+//	
+//	public Word(float x, float y, float speed, Color color, String word) {
+//        super(x, y, color, speed);
+//        this.word = word;
+//	
+//	}
+//
+//	public String getWord() {
+//		return word;
+//	}
+//
+//	public void setWord(String word) {
+//		this.word = word;
+//	}
+//	
+//
+//}
+//
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Word extends Entity {
+    private String word;
+    private BitmapFont font;
+    private float scale; // Font scale
 
-	// string
-	private String word;
-	
-	public Word(float x, float y, float speed, Color color, String word) {
+    public Word(float x, float y, float speed, Color color, String word, float scale) {
         super(x, y, color, speed);
         this.word = word;
-	
-	}
+        this.scale = scale;
 
-	public String getWord() {
-		return word;
-	}
+        // ✅ Initialize BitmapFont and Apply Scaling
+        font = new BitmapFont();
+        font.getData().setScale(scale); // Adjust font size
+        font.setColor(color);
+    }
 
-	public void setWord(String word) {
-		this.word = word;
-	}
-	
+    public String getWord() {
+        return word;
+    }
 
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public void draw(SpriteBatch spriteBatch) {
+        font.getData().setScale(scale); // Ensure scaling is applied
+        font.draw(spriteBatch, word, getX(), getY());
+    }
 }
+
